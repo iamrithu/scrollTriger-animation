f5cb7ea28f6449c6677334fe1e4eb1c796f9aea2
rithi <ritimahesh29@gmail.com>
2022-03-25 18:04:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,601 @@
+gsap.registerPlugin(ScrollTrigger)
+
+
+gsap.to("#indroText", {
+
+    y: -1000,
+
+
+    // duration: 2,
+    scrollTrigger: {
+
+        start: 100,
+        end: 300,
+        scrub: 1
+
+    }
+})
+
+gsap.to("#welcomeText", {
+
+    y: -500,
+
+
+    // duration: 2,
+    scrollTrigger: {
+
+        start: 100,
+        end: 300,
+        scrub: 1
+
+    }
+})
+
+gsap.to("#spaceShip", {
+
+    x: 300,
+
+    // duration: 2,
+    scrollTrigger: {
+
+        start: 100,
+        end: 300,
+        scrub: 1
+
+    }
+})
+
+gsap.to("#spaceShip", {
+    x: 4600,
+    y: -1300,
+    scale: 30,
+
+    // duration: 2,
+    scrollTrigger: {
+
+        start: 400,
+        end: 700,
+        scrub: 1
+
+    }
+})
+gsap.to("#welcomeText", {
+
+    opacity: 0,
+
+
+    // duration: 2,
+    scrollTrigger: {
+
+        start: 400,
+        end: 500,
+        scrub: 1
+
+    }
+})
+
+gsap.to("#spaceShip", {
+    opacity: 0,
+    // duration: 0.5,
+    scrollTrigger: {
+        start: 700,
+        end: 800,
+        scrub: 1
+    }
+})
+gsap.to("#spaceFrame", {
+    opacity: 1,
+    // duration: 2,
+    scrollTrigger: {
+        start: 700,
+        end: 1000,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#planet", {
+
+    opacity: 1,
+    // duration: 0.6,
+    // delay: 1,
+    scrollTrigger: {
+
+        start: 800,
+        end: 1000,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text1", {
+
+    opacity: 1,
+    duration: 0.6,
+    delay: 1,
+    scrollTrigger: {
+
+        start: 850,
+        end: 1000,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#spaceFrame", {
+    scale: 10,
+    scrollTrigger: {
+        start: 1200,
+        end: 1500,
+        scrub: 1
+    }
+})
+gsap.to("#text1", {
+
+    opacity: 0,
+    duration: 0.6,
+    delay: 0.6,
+    scrollTrigger: {
+        start: 1200,
+        end: 1300,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#planet", {
+
+    scale: 3,
+
+    x: -150,
+    rotate: 250,
+    scrollTrigger: {
+        start: 1550,
+        end: 1800,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#content", {
+
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 1700,
+        end: 2000,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#planet", {
+
+
+
+
+    scale: 30,
+
+    scrollTrigger: {
+        start: 2000,
+        end: 2300,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#content", {
+
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 2100,
+        end: 2200,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#planet", {
+
+
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 2400,
+        end: 2600,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#forest", {
+
+
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 2500,
+        end: 2700,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#forest-cover", {
+
+
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 2600,
+        end: 2800,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text2", {
+
+
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 2600,
+        end: 2800,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text2", {
+
+
+    y: -500,
+
+    scrollTrigger: {
+        start: 2800,
+        end: 3000,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#city", {
+
+
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 2900,
+        end: 3200,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text3", {
+
+
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 3100,
+        end: 3300,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#city", {
+
+    x: 2800,
+    scale: 10,
+
+    scrollTrigger: {
+        start: 3400,
+        end: 3700,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text3", {
+
+    x: -100,
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 3500,
+        end: 3600,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#city", {
+
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 3700,
+        end: 3900,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#theatre", {
+
+    opacity: 1,
+
+
+    scrollTrigger: {
+        start: 3800,
+        end: 4000,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text4", {
+
+    opacity: 1,
+    y: -150,
+
+
+    scrollTrigger: {
+        start: 4000,
+        end: 4100,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text4", {
+
+    opacity: 1,
+    y: -1000,
+
+
+    scrollTrigger: {
+        start: 4200,
+        end: 4300,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#theatre", {
+
+    scale: 1.5,
+
+
+
+    scrollTrigger: {
+        start: 4200,
+        end: 4300,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#glow-globle", {
+
+    opacity: 1,
+
+
+
+    scrollTrigger: {
+        start: 4250,
+        end: 4350,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#glow-globle", {
+
+    opacity: 0,
+
+
+
+    scrollTrigger: {
+        start: 4330,
+        end: 4350,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#vector", {
+
+    opacity: 1,
+
+
+
+    scrollTrigger: {
+        start: 4250,
+        end: 4350,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#Q-1", {
+
+    opacity: 1,
+
+
+
+    scrollTrigger: {
+        start: 4300,
+        end: 4400,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#road-map", {
+
+    opacity: 1,
+
+
+
+    scrollTrigger: {
+        start: 4300,
+        end: 4400,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text5", {
+
+    opacity: 1,
+
+
+
+    scrollTrigger: {
+        start: 4300,
+        end: 4400,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#road-map", {
+
+
+    scale: 2,
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 4500,
+        end: 4700,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#title", {
+
+
+
+    opacity: 1,
+    y: 50,
+
+    scrollTrigger: {
+        start: 4500,
+        end: 4700,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#text5", {
+
+
+    x: 300,
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 4500,
+        end: 4700,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#Q-1", {
+
+
+    x: 300,
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 4500,
+        end: 4700,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#vector", {
+
+
+    x: -300,
+    opacity: 0,
+
+    scrollTrigger: {
+        start: 4500,
+        end: 4700,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#conclusion", {
+
+
+    y: -50,
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 4600,
+        end: 4800,
+        scrub: 1
+
+    }
+
+
+})
+gsap.to("#input-value", {
+
+
+    x: -300,
+    y: 100,
+    opacity: 1,
+
+    scrollTrigger: {
+        start: 4600,
+        end: 4800,
+        scrub: 1
+
+    }
+
+
+})
